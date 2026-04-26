f27922fd473c248cc5ca92b77507790743cd2a88
mgos-faustinius <xv0594@lunet.lboro.ac.uk>
2026-04-26 14:11:49 +0100

    # feat(sensor): add temperature data collection and buffering
    - Record analogRead from sensor and convert to temp in Celsius
    - Store readings in temp data array using index and while loop
    - Use 1 Hz sampling for active mode, adjustable for future modes

--- arduino/Arduino Task/src/loovee.ino
+++ arduino/Arduino Task/src/loovee.ino
@@ -5,6 +5,24 @@ const int B = 4275000; // B value of the thermistor
 const int R0 = 100000; // R0 = 100k
 const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
 
+//my variables lmao
+int temperature_data_array[180]; // array to store temperature data to process in DFT
+int sampling_rate = 1000; // sampling rate in ms, changeable, active mode first so we need 1hz first
+
+void collect_temperature_data(){ //collects data and stores it in temperature_data_array
+  unsigned long start_time = millis();
+  int index = 0; // index for storing data in array
+  while (millis() - start_time < 180000 && index < 180) { // collect data for 3 minutes at the start (180000 ms), && prevents index overflow
+    int a = analogRead(pinTempSensor);
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // temperature calculations
+    temperature_data_array[index] = temperature; // store temperature data in array using index variable!
+    index++;
+    delay(sampling_rate);
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
