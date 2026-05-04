4c8416700a535ae1e5c8a0453533a6628e4f30f7
mgos-faustinius <xv0594@lunet.lboro.ac.uk>
2026-05-04 16:46:57 +0100

    # feat(temp_sensor): added send_data_to_pc function and decide power mode function  - prints data in form of time, temperature, Frequency, and magnitude  - decide power mode based on if frequency is between 0.5 or 0.1 to decide power modes
    fix: changed variable temp_data_array to float to increase decimal precision and array size to 720 to handle largest possible sample
    
    fix: numSamples start at 0

--- arduino/Arduino Task/src/temperature_optimization.ino
+++ arduino/Arduino Task/src/temperature_optimization.ino
@@ -6,32 +6,39 @@ const int R0 = 100000; // R0 = 100k
 const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
 
 //my variables lmao
-int temperature_data_array[180]; // array to store temperature data to process in DFT
+float temperature_data_array[720]; // array to store temperature data to process in DFT
 int sampling_rate = 1000; // sampling rate in ms, changeable, active mode first so we need 1hz first
-int numSamples = 180; // number of samples to collect, active mode first so we need 180 samples for 3 min at 1hz
+int numSamples = 0; // number of samples to collect
 float real[720]; // array to store real part of DFT results, size to handle worse case 4hz for 3 min, which is 720 samples
 float imag[720]; // array to store imaginary part of DFT results 
 float magnitude[720]; // array to store magnitude of DFT calculations
 float fk; // variable to store frequency of dominant frequency component
+unsigned long sample_time[720];
+const int ACTIVE = 0;
+const int IDLE = 1;
+const int POWER_DOWN = 2;
+int power_mode = ACTIVE; // variable to track current power mode, start in active mode
 
 void collect_temperature_data(){ //collects data and stores it in temperature_data_array
   unsigned long start_time = millis();
   int index = 0; // index for storing data in array
-  while (millis() - start_time < 180000 && index < numSamples) { // collect data for 3 minutes at the start (180000 ms), && prevents index overflow
+  
+  while (millis() - start_time < 180000 && index < 720) { // collect data for 3 minutes at the start (180000 ms), && prevents index overflow
     int a = analogRead(pinTempSensor);
     float R = 1023.0/a-1.0;
     R = R0*R;
     float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // temperature calculations
     temperature_data_array[index] = temperature; // store temperature data in array using index variable!
+    sample_time[index] = millis(); // store sample time
     index++;
     delay(sampling_rate);
   }
-  magnitude[index] = 0;
+  numSamples = index; // update numSamples to reflect actual number of samples collected
 }
 
 float* apply_dft(){
  int bigIndex = 1; // variable to track index of largest magnitude, start at 1 to ignore DC component
- int sampling_rateHz = 1000/sampling_rate; // convert sampling rate from ms to Hz
+ float sampling_rateHz = 1000.0/sampling_rate; // variable to store sampling rate in Hz, calculated from sampling_rate in ms
  for (int i = 0; i < numSamples; i++){ // zero out real and imag arrays to 0 before DFT calculations
     real[i] = 0;
     imag[i] = 0;
@@ -51,7 +58,32 @@ float* apply_dft(){
 return &fk; // return pointer to frequency of dominant frequency component
 }
 
+void send_data_to_pc(){
+  float sampling_rateHz = 1000.0/sampling_rate;
+  for (int i = 0; i < numSamples; i++){
+   Serial.print("Time: ");
+   Serial.print(sample_time[i]);
+   Serial.print(" ms, Temperature: ");
+   Serial.print(temperature_data_array[i]);
+   Serial.print(" °C, Frequency: ");
+   Serial.print(i * sampling_rateHz / numSamples);
+   Serial.print(" Hz, Magnitude: ");
+   Serial.println(magnitude[i]);
+  }
+}
 
+int moving_average(int data[], int size, int window) {
+  
+}
+
+int decide_power_mode(){
+ if (fk <= 0.1){ // if dominant frequency is less than 0.1 Hz, we can go to sleep mode
+   return POWER_DOWN;
+ } else if (fk > 0.5){ // if dominant frequency is greater than 0.5 hz, we go to active mode
+   return ACTIVE;
+ } else { // if dominant frequency is between 0.1 and 0.5 Hz, go to idle mode
+   return IDLE;
+ }
 
 void setup()
 {
